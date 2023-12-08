f626ed44c54bc62220c7d62ddafbd46d04cf502a
Tade0G <tadeogomez1988@gmail.com>
2023-12-07 22:21:15 -0300

    agregando configuraciones al servidor

--- src/app.js
+++ src/app.js
@@ -4,3 +4,19 @@ let app = express();
 
 //requerimos path
 let path=require("path");
+
+//definimos el puerto
+let puerto="1000";
+
+//creamos servidor
+app.listen(puerto,aviso);
+
+//definimos los lugares de archivos estaticos
+app.use(express.static("public"));
+
+//funcion: aviso
+//by: Tade0G
+//def: indica el estado del servidor y su puerto
+function aviso(){
+	console.log("servidor corriendo en el puerto " + puerto);
+}
